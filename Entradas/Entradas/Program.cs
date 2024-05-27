aaaea3bba870a6e9f1571f2a8093c4ab2bdd2fa8
LuisferChinchilla <luis.chinchilla96@gmail.com>
2024-05-26 22:37:03 -0600

    Ejercicios de la tarea 2

---
+++ Entradas/Entradas/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entradas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este ejercicio me quedo incompleto profe, no pude hacer que cumpla todo lo solicitado, le dedique mucho tiempo y nada,
+            //estoy cerrado de mente ya de tanto intentar =(
+            string cedula;
+            string nombre;
+            int localidad;
+            int cantidad_entradas = 0;
+            int opcion = 0;
+            int precio;
+            int total;
+            int cargos;
+            string nombre_localidad;
+
+            Console.WriteLine("Bienvenido, gracias por su interes en comprar entradas para el partido CR - MX");
+            Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+            opcion = int.Parse(Console.ReadLine());
+            while (opcion != 2)
+            {   Console.Write("Por favor digite el número de entradas que desea comprar: ");
+                cantidad_entradas = int.Parse(Console.ReadLine());
+                if (cantidad_entradas > 4)
+                {
+                    Console.WriteLine("**ERROR**No se permite la compra mayor a 4 entradas por usuario.");
+                    Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                Console.Write("Por favor digite un número de acuerdo a la localidad:\n 1- Sol Norte/Sur ¢10.500\n 2- Sombra Este/Oeste ¢20500\n 3- Preferencial ¢25500 ");
+                localidad = int.Parse(Console.ReadLine());
+               
+                if (cantidad_entradas <= 4 & localidad == 1)
+                {
+                    Console.Write("Digite su nombre: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Digite su número de cédula: ");
+                    cedula = Console.ReadLine();
+                    precio = (10500 * cantidad_entradas);
+                    cargos = (cantidad_entradas * 1000);
+                    total = (precio + cargos);
+                    nombre_localidad = "Sol Norte/Sur";
+                    Console.WriteLine("Transaccion exitosa.\nDetalles: Compra a nombre de: " + nombre + "\nCédula: " + cedula + " Localidad:" + nombre_localidad + " Subtotal:" + precio + " Total:" + total);
+                    Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                else if (cantidad_entradas <= 4 & localidad == 2)
+                {
+                    Console.Write("Digite su nombre: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Digite su número de cédula: ");
+                    cedula = Console.ReadLine();
+                    precio = (20500 * cantidad_entradas);
+                    cargos = (cantidad_entradas * 1000);
+                    total = (precio + cargos);
+                    nombre_localidad = "Sombra Este/Oeste";
+                    Console.WriteLine("Transaccion exitosa.\n Detalles: Compra a nombre de: " + nombre + "\nCédula: " + cedula + " Localidad: " + nombre_localidad + " Subtotal: " + precio + " Total: " + total);
+                    Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+                    opcion = int.Parse(Console.ReadLine());
+
+                }
+                else if (cantidad_entradas <= 4 & localidad == 3)
+                {
+                    Console.Write("Digite su nombre: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Digite su número de cédula: ");
+                    cedula = Console.ReadLine();
+                    precio = (25500 * cantidad_entradas);
+                    cargos = (cantidad_entradas * 1000);
+                    total = (precio + cargos);
+                    nombre_localidad = "Preferencial";
+                    Console.WriteLine("Transaccion exitosa.\n Detalles: Compra a nombre de: " + nombre + "\nCédula: " + cedula + " Localidad: " + nombre_localidad + " Subtotal: " + precio + " Total: " + total);
+                    Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+                    opcion = int.Parse(Console.ReadLine());
+
+                }
+                else{
+
+                    Console.WriteLine("**ERROR**No se permite la compra mayor a 4 entradas por usuario.");
+                    Console.WriteLine("Eliga una opcion:\n1- Comprar entradas.\n2- Salir. ");
+                    opcion = int.Parse(Console.ReadLine());
+                    Console.Read();
+                }
+
+
+            }
+             Console.Read();
+            
+        }
+        
+    }
+}
---
+++ tarea2_Ejercicios/tarea2_Ejercicios/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea2_Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string cedula;
+            double aumento;
+            double salario_bruto;
+            double salario_neto;
+            double deducciones;
+            int tipo_empleado;
+            double cantidad_horas;
+            double precio_hora;
+            double salario_ordinario;
+            int cantidad;
+            int conO = 0;
+            int conT = 0;
+            int conP = 0;
+            double acumuladoNeto_Operario = 0;
+            double acumuladoNeto_Tecnico = 0;
+            double acumuladoNeto_Profesional = 0;
+            Console.Write("Este programa brinda informacion sobre los aumentos de salario de empleados.\nDigite la cantidad de empleados que desea consultar: ");
+            cantidad = int.Parse(Console.ReadLine());
+            int[] datos = new int[cantidad];
+            for (int i = 0; i < datos.Length; i++)
+            {
+                Console.Write("Escriba el nombre: "); nombre = Console.ReadLine();
+                Console.Write("Digite el numero de cédula: "); cedula = Console.ReadLine();
+                //Console.Write("Tipos de empleado:\n1-Operario\n2-Técnico\n3-Profesional\nDigite un número de acuerdo al tipo de empleado: ");
+                //tipo_empleado = int.Parse(Console.ReadLine());
+                Console.Write("Digite la cantidad de horas laboradas "); cantidad_horas = double.Parse(Console.ReadLine());
+                Console.Write("Digite el precio por hora: "); precio_hora = double.Parse(Console.ReadLine());
+                salario_ordinario = (cantidad_horas * precio_hora);
+                Console.Write("Tipos de empleado:\n1-Operario\n2-Técnico\n3-Profesional\nDigite un número de acuerdo al tipo de empleado: ");
+                tipo_empleado = int.Parse(Console.ReadLine());
+                if (tipo_empleado == 1)
+                {
+                    aumento = (salario_ordinario * 0.15);
+                    salario_bruto = (salario_ordinario + aumento);
+                    deducciones = (salario_bruto * 9.17 / 100);
+                    salario_neto = (salario_bruto - deducciones);
+                    conO += 1;
+                    acumuladoNeto_Operario += salario_neto;
+                    Console.WriteLine("Los datos del empleado: " + nombre + " cedula " + cedula + " son los siguientes:\nSalario Ord ¢" + salario_ordinario + " el aumento es de ¢" + aumento + " las deducciones ¢" + deducciones + " el salario neto es de ¢" + salario_neto);
+                    Console.Read();
+                }
+                else if (tipo_empleado == 2)
+                {
+                    aumento = (salario_ordinario * 0.10);
+                    salario_bruto = (salario_ordinario + aumento);
+                    deducciones = (salario_bruto * 9.17 / 100);
+                    salario_neto = (salario_bruto - deducciones);
+                    conT += 1;
+                    acumuladoNeto_Tecnico += salario_neto;
+                    Console.WriteLine("Los datos del empleado: " + nombre + " cedula " + cedula + " son los siguientes:\nSalario Ord ¢" + salario_ordinario + " el aumento es de ¢" + aumento + " las deducciones ¢" + deducciones + " el salario neto es de ¢" + salario_neto);
+                    Console.Read();
+                }
+                else if (tipo_empleado == 3)
+                {
+                    aumento = (salario_ordinario * 0.05);
+                    salario_bruto = (salario_ordinario + aumento);
+                    deducciones = (salario_bruto * 9.17 / 100);
+                    salario_neto = (salario_bruto - deducciones);
+                    conP += 1;
+                    acumuladoNeto_Profesional += salario_neto;
+                    Console.WriteLine("Los datos del empleado: " + nombre + " cedula " + cedula + " son los siguientes:\nSalario Ord ¢" + salario_ordinario + " el aumento es de ¢" + aumento + " las deducciones ¢" + deducciones + " el salario neto es de ¢" + salario_neto);
+                    Console.Read();
+                } // Console.WriteLine("Acumulado de salarios netos de acuerdo al tipo: 
+
+                Console.Read();
+
+            }
+            Console.WriteLine("Datos adicionales: \nCantidad empleados de acuerdo al tipo: \nOperario: " + conO + "\nTécnico: " + conT + "\nProfesional: " + conP);
+            Console.WriteLine("Acumulado de salarios netosde acuerdo al tipo: \nOperario:¢ " + acumuladoNeto_Operario + "\nTécnico:¢ " + acumuladoNeto_Tecnico + "\nProfesional:¢ " + acumuladoNeto_Profesional);
+            Console.Read();
+
+        }
+    }
+}
